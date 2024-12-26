a7f9525b58a3afd33be4e36c777c178fabc3b236
sologixenergy <sologixdevelopment@gmail.com>
2024-12-26 15:04:23 +0530

    Update vars.js

--- src/config/vars.js
+++ src/config/vars.js
@@ -1,16 +1,6 @@
 const path = require("path");
 const dotenv = require("dotenv").config();
-const dotenvExample = require("dotenv").config({
-  path: path.resolve(process.cwd(), ".env.example"),
-});
 
-if (
-  Object !== null &&
-  JSON.stringify(Object.keys(dotenv.parsed).sort()) !==
-    JSON.stringify(Object.keys(dotenvExample.parsed).sort())
-) {
-  throw Error("Missing values in .env. Please refer to .env.example");
-}
 
 module.exports = {
   port: process.env.PORT,
